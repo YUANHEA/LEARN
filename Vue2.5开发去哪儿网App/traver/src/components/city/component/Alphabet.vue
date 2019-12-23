e604b2634e467c6308fd7eae6df2876daa94ab58
YuanHe <15220918751@163.com>
2019-12-23 20:27:48 +0800

    主支能pull更新到代码
    1

--- Vue2.5开发去哪儿网App/traver/src/components/city/component/Alphabet.vue
+++ Vue2.5开发去哪儿网App/traver/src/components/city/component/Alphabet.vue
@@ -28,7 +28,7 @@ export default {
   // 因此对于固定数值量，写到update生命周期函数中
   updated () {
     this.startY = this.$refs['A'][0].offsetTop
-    this.timer = 0 // 在特点支修改内容，改动会被当前分支检测到，其它分支没有影响
+    this.timer = 1 // 修改页面内容
   },
   computed: {
     // 需要根据数组下表去对应字母，先将数据如果是对象类型，先转为数组
