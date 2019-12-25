1602eb89d2ae1832b1eaa0e6d4d154c9eab5964a
YuanHe <15220918751@163.com>
2019-12-25 15:53:40 +0800

    keepalive
    keepalive标签，给router-view做缓存
    keepAlive会将已经加载过的接口数据保存到缓存中
    axios在不同业务参数应该，应该带参数且拼接不同参数
    axios在不同业务参数应该，应该带参数且拼接不同参数，请求接口文件中不同内容
    业务参数不同，再次发axios请求
    利用activated生命周期与临时缓存，实现在keepalive运用情况下，再次调用axios方法
    先将数据量赋值给临时缓存量
    到actived生命周期钩子中，比较新量与临时缓存量是否相同，不同则再次调axios方法

--- Vue2.5开发去哪儿网App/traver/src/App.vue
+++ Vue2.5开发去哪儿网App/traver/src/App.vue
@@ -3,7 +3,7 @@
     <!-- router-view显示的是当前路由地址所对应内容 -->
     <!-- 当前路由地址对应内容为helloworld小组件 -->
     <!-- 当前路由地址怎么找？到父组件中看router，会再找到router文件夹中index.js文件中路由配置项，就能知道路由地址对于内容 -->
-    <router-view/>
+    <keep-alive><router-view/></keep-alive>
   </div>
 </template>
 
--- Vue2.5开发去哪儿网App/traver/src/components/home/Home.vue
+++ Vue2.5开发去哪儿网App/traver/src/components/home/Home.vue
@@ -21,6 +21,7 @@ import HomeNav from './component/nav' // 导入分类导航组件
 import HomeRecomment from './component/Recomment' // 导入热销推荐组件
 import HomeWeekend from './component/Weekend' // 导入周末游组件组件
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'home',
   components: {
@@ -33,19 +34,24 @@ export default {
   },
   data () {
     return {
-      city: '', // 城市
+      // city: '', // 城市
       swiperList: [],
       iconList: [],
       recommendList: [],
-      weekendList: []
+      weekendList: [],
+      tempCity: '' // city公共量临时缓存量
     }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     // 先axios接受数据，再通过父组件传值给子组件，分数据到各个子组件
     getHomeInf: function () {
       // 在methors中调用方法不用方法名，因为同为对象？
       // 一个页面一个接口请求
-      axios.get('/api/index.json').then(this.getHomeInfSucc)
+      axios.get('/api/index.json?city=' + this.city)
+        .then(this.getHomeInfSucc)
     },
     getHomeInfSucc: function (res) {
       console.log('数据', res)
@@ -56,7 +62,7 @@ export default {
       if (res.ret && res.data) {
         // 将接受到的数据先保存到变量中，再调用变量
         const dat = res.data
-        this.city = dat.city
+        // this.city = dat.city
         this.swiperList = dat.swiperList
         this.iconList = dat.iconList
         this.recommendList = dat.recommendList
@@ -66,7 +72,18 @@ export default {
   },
   // 在mounted方法中调用axios方法
   mounted () {
+    this.tempCity = this.city
     this.getHomeInf()
+  },
+  // activated生命周期钩子
+  // 在用keepAlive缓存情况，用keepAlive提供的activated生命周期钩子，在activated实现对axois再次请求。
+  // 作用，和computed计算属性相同，请求axios
+  activated () {
+    // console.log('activated')
+    if (this.tempCity !== this.city) {
+      this.tempCity = this.city
+      this.getHomeInf()
+    }
   }
 }
 </script>
